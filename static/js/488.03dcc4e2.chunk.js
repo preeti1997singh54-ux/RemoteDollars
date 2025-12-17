e1b393c2488f82bfb5dc9f8384ca86c59d5f0d7a
Preeti Singh <PREETI1997SINGH54@GMAIL.COM>
2025-12-18 03:21:20 +0530

    Updates

--- static/js/488.03dcc4e2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkremotedollars_landing=self.webpackChunkremotedollars_landing||[]).push([[488],{488:(e,a,l)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.03dcc4e2.chunk.js.map
---
+++ static/js/488.b4b93246.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkremotedollars_upgraded=self.webpackChunkremotedollars_upgraded||[]).push([[488],{488:(e,a,r)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.b4b93246.chunk.js.map
